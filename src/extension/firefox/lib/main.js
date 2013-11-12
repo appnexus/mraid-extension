001692fbd63bfab4a8afc1d4b3569892f24567a5
Mike Chevett <mchevett@appnexus.com>
2013-11-12 15:36:32 -0500

    hide/show the firefox icon based on extension activity

--- src/extension/firefox/lib/main.js
+++ src/extension/firefox/lib/main.js
@@ -2,12 +2,15 @@ var observer = require('observer-service'),
 	widgets = require('sdk/widget'),
 	pageMod = require('sdk/page-mod'),
 	ss = require('sdk/simple-storage'),
+	tabs = require('sdk/tabs'),
 	thisExtension = require('self'),
 	chrome = require('chrome'),
 	Cc = chrome.Cc,
 	Ci = chrome.Ci;
 
-var mraidUrl = 'ANX_MRAID_URL';
+var mraidUrl = 'ANX_MRAID_URL',
+	tabIconLookup = {};
+
 
 observer.add('http-on-modify-request', function(subject) {
 	subject.QueryInterface(Ci.nsIHttpChannel);
@@ -16,6 +19,10 @@ observer.add('http-on-modify-request', function(subject) {
 	if (typeof url !== 'string' || !/mraid\.js($|\?)/.test(url) || url === mraidUrl) return;
 	
 	console.log('redirecting to "' + mraidUrl + '".');
+
+	tabIconLookup[tabs.activeTab.id] = true;
+	showOrHideIcon();
+
 	var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);  
 	var uri = ios.newURI(mraidUrl, null, null); 
 
@@ -36,10 +43,41 @@ pageMod.PageMod({
 	}
 });
 
-widgets.Widget({
-	id: "adnxs-mraid-link",
+
+var myWidget =  widgets.Widget({
+	id: "anx-mraid-extension-icon",
 	label: "AppNexus MRAID Viewer",
-	contentURL: thisExtension.data.url('icon128.png'),
-	onClick: function() {}
+	content: '&nbsp;'
 });
 
+// this doesn't "mod the page", instead we just need 
+// to know when a page load is starting in order to hide/show the
+// appnexus icon
+pageMod.PageMod({
+	include: '*',
+	attachTo: ['top'],
+	contentScript: ';0;',
+	contentScriptWhen: 'start',
+	onAttach: function(){
+		tabIconLookup[tabs.activeTab.id] = false;
+		showOrHideIcon();
+	}
+});
+
+tabs.on('open', showOrHideIcon);
+tabs.on('activate', showOrHideIcon);
+tabs.on('deactivate', showOrHideIcon);
+tabs.on('close', function(tab){ delete tabIconLookup[tab.id]; });
+
+function showOrHideIcon(tab){
+	tab = tab || tabs.activeTab;
+
+	var show = tabIconLookup[tab.id];
+	var view = myWidget.getView(tabs.activeTab.window);
+
+	if (show){
+		view.content = '<img src="' + thisExtension.data.url('icon128.png') + '" />';
+	} else {
+		view.content = '&nbsp;';
+	}
+}
